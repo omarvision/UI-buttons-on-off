9f95a4eb3de8c5be3e37ef352aea27702ace2a28
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 12:57:54 -0400

    Add files via upload

---
+++ ButtonCommands.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for BUttons
+
+public class ButtonCommands : MonoBehaviour
+{
+    public Button btnOne;
+    public Button btnTwo;
+
+    public void PressedButtonOne()
+    {
+        btnTwo.gameObject.SetActive(false);
+        Invoke("btnTwoAppear", 3.0f);
+    }
+    public void PressedButtonTwo()
+    {
+        btnOne.gameObject.SetActive(false);
+        Invoke("btnOneAppear", 3.0f);
+    }
+
+    private void btnOneAppear()
+    {
+        if (btnOne.gameObject.activeSelf == false)
+        {
+            btnOne.gameObject.SetActive(true);
+        }
+    }
+    private void btnTwoAppear()
+    {
+        if (btnTwo.gameObject.activeSelf == false)
+        {
+            btnTwo.gameObject.SetActive(true);
+        }
+    }
+}
